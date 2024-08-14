f9f2df5922af4e08a29a21ac5b6335dedb7e99e6
Kacper <webbykacper@gmail.com>
2024-08-14 08:43:23 +0200

    Correction

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# [0.0.3] 
+    - responsiveness correction (max 767 px)
+
 # [0.0.2]
     - responsiveness added (max-width: 767px);
     - add mobile menu container
--- client/src/components/opinions.jsx
+++ client/src/components/opinions.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import avatar from "../assets/images/author's-avatar.png";
 
 export default function Opinions() {
-    const text = "Zakupy w City Kicks to czysta przyjemność. Mają naprawdę imponujący asortyment butów – od klasycznych sneakersów po najnowsze modele od topowych marek. Plus za szybkie realizowanie zamówień i konkurencyjne ceny. Na pewno będę wracać!";
+    const text = "Zakupy w City Kicks to czysta przyjemność. Mają naprawdę imponujący asortyment butów – od klasycznych sneakersów po najnowsze modele od topowych marek. Plus za szybkie realizowanie zamówień. Na pewno będę wracać!";
     return (
         <section className="opinions">
             <h1> Opinie naszych klientów </h1>

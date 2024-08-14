4902c8c57137a5b3fe78baa5cd80e9f4a8cc3b8b
Kacper <webbykacper@gmail.com>
2024-08-14 09:03:03 +0200

    Offer file import was corrected

--- client/src/routes/home.jsx
+++ client/src/routes/home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../components/header";
-import Offer from "../components/Offer";
+import Offer from "../components/offer";
 import Newsletter from "../components/newsletter";
 import Opinions from "../components/opinions";
 import Footer from "../components/footer";
